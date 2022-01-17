88be8e961638696b4e18a6ac7cabb93962218b86
Mauro Frischherz <mauro.frischherz@gmail.com>
2022-01-17 18:20:02 +0100

    basic layout

--- lib/data_grid.dart
+++ lib/data_grid.dart
@@ -1,7 +1,93 @@
 library data_grid;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class DataGrid extends StatelessWidget {
+  const DataGrid(
+      {Key? key,
+      required this.columns,
+      required this.rows,
+      this.onValueChanged})
+      : super(key: key);
+
+  final List<GridColumn> columns;
+  final List<GridRow> rows;
+
+  final Function(
+    int col,
+    int row,
+    dynamic value,
+  )? onValueChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      final cellWidth = constraints.maxWidth / columns.length;
+      return DataTable(
+        horizontalMargin: 0,
+        checkboxHorizontalMargin: 0,
+        columnSpacing: 0,
+        columns: columns.map((e) => DataColumn(label: e.label)).toList(),
+        rows: [
+          for (int r = 0; r < rows.length; r++)
+            DataRow(cells: [
+              for (int c = 0; c < columns.length; c++)
+                _buildCell(r, c, cellWidth)
+            ]),
+        ],
+      );
+    });
+  }
+
+  DataCell _buildCell(int rowIndex, int columnIndex, double? cellWidth) {
+    final row = rows[rowIndex];
+
+    switch (columns[columnIndex].type) {
+      case DataType.text:
+        return DataCell(SizedBox(
+          child: Text(row.cells[columnIndex]),
+          width: cellWidth,
+        ));
+      case DataType.textFormField:
+        return DataCell(SizedBox(
+          child: TextFormField(
+              initialValue: row.cells[columnIndex],
+              onChanged: (value) {
+                if (onValueChanged != null) {
+                  onValueChanged!(columnIndex, rowIndex, value);
+                }
+              }),
+          width: cellWidth,
+        ));
+      case DataType.checkbox:
+        return DataCell(SizedBox(
+          child: Checkbox(
+              value: row.cells[columnIndex],
+              onChanged: (value) {
+                if (onValueChanged != null) {
+                  onValueChanged!(columnIndex, rowIndex, value);
+                }
+              }),
+          width: cellWidth,
+        ));
+      case DataType.widget:
+        return DataCell(
+            SizedBox(child: row.cells[columnIndex], width: cellWidth));
+    }
+  }
 }
+
+class GridRow {
+  final List<dynamic> cells;
+
+  GridRow(this.cells);
+}
+
+class GridColumn {
+  final Widget label;
+  final DataType type;
+
+  GridColumn(this.label, this.type);
+}
+
+enum DataType { text, textFormField, checkbox, widget }
